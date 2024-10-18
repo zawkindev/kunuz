57842929977c9861fc8abc72db7ad3946f3c0fd9
zawkindev <zawkindev@gmail.com>
2024-10-19 01:16:56 +0500

    feat: add spring

---
+++ src/main/java/zawkin/asuna/kunuz/KunuzApplication.java
@@ -0,0 +1,13 @@
+package zawkin.asuna.kunuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zawkin/asuna/kunuz/KunuzApplicationTests.java
@@ -0,0 +1,13 @@
+package zawkin.asuna.kunuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
