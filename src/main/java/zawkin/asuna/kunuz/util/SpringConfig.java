54bc1787b786dfff33a7e60ec601ce4da32fa704
zawkindev <zawkindev@gmail.com>
2024-11-07 14:43:25 +0500

    feat: default implementation

---
+++ src/main/java/zawkin/asuna/kunuz/util/SpringConfig.java
@@ -0,0 +1,49 @@
+package zawkin.asuna.kunuz.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfig {
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        String password = UUID.randomUUID().toString();
+        System.out.println("User password: " + password);
+
+        UserDetails user = User.builder()
+                .username("asuna")
+                .password("{noop}" + password)
+                .roles("USER")
+                .build();
+
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(user));
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .anyRequest()
+                    .authenticated();
+        }).formLogin(Customizer.withDefaults());
+
+        http.csrf(Customizer.withDefaults());
+        http.cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
